d37037d528ba7f444fd08ce66dd0e80f5950d1d7
ThamizhAnban <thamizhanban2006@gmail.com>
2025-03-06 11:51:53 +0530

    Completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,70 @@ import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
 import Usercard from './components/usercard.jsx';
+import img from './assets/Ben 10.png'
 
 function App() {
-  const cards = Array.from({ length: 10 });
+  const cards = [
+    {
+      name:"Shankar",
+      email:"shankar@gamil.com",
+      phone_no:12345678,
+      address:"36, gandhi street, chennai",
+      image:img
+    },
+    {
+      name:"Abishek",
+      email:"abishek@gamil.com",
+      phone_no:12345678,
+      address:"36, Nehru street, chennai",
+      image:img,
+    },
+    {
+      name:"Lithish",
+      email:"lithish@gamil.com",
+      phone_no:12345678,
+      address:"36,  mosque street, chennai",
+      image:img,
+    },
+    {
+      name:"Rohan",
+      email:"rohan@gamil.com",
+      phone_no:12345678,
+      address:"36, dark street, chennai",
+      image:img,
+    },
+    {
+      name:"Joe Celaster",
+      email:"celaster@gamil.com",
+      phone_no:12345678,
+      address:"36, Church street street, chennai",
+      image:img,
+    },
+
+
+
+
+  ]
   const gridStyle = {
     display: 'grid',
     gridTemplateColumns: 'repeat(4, 1fr)',
-    gap: '16px',
-    padding: '16px',
+    gap: '20px',
+    padding: '20px',
   };
+  // const box = {
+  //   padding:'10px',
+  //   border:'2px solid black',
+  //   padding:'20px',
+  //   height:'300px',
+  //   width:'200px',
+    
+
+  // }
 
   return (
     <div style={gridStyle}>
-      {cards.map((_, index) => (
-        <Usercard key={index} />
+      {cards.map((i, index) => (
+        <Usercard key={index} name={i.name} email={i.email} pno={i.phone_no} address={i.address} image={i.image} />
       ))}
     </div>
   );
--- src/components/usercard.jsx
+++ src/components/usercard.jsx
@@ -1,1 +1,29 @@
 // write the component code here
+import React from 'react';
+import '../App.css';
+
+const Usercard = ({name,email,pno,address,image,style}) => {
+    return (
+        <div  className="box">
+            <div className='profile'>
+                <img  src={image} alt='image'/>
+            </div>
+            <div className='name'>
+                <p><strong>Name:</strong>{name}</p>
+            </div>
+            <div className='email'>
+                <p><strong>Email:</strong>{email}</p>
+            </div>
+            <div className="pno">
+                <p><strong>Phone No:</strong>{pno}</p>
+            </div>
+            <div className='address'>
+                <p><strong>Address:</strong>{address}</p>
+            </div>
+
+
+        </div>
+    );
+}
+
+export default Usercard;
